22be0a427a02cac0fd585cc570e9d95b2636aac4
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-09-02 15:20:44 -0300

    feat: create edit and delete methods userService

--- src/app/components/services/users.service.ts
+++ src/app/components/services/users.service.ts
@@ -11,6 +11,7 @@ export class UsersService {
 
   private readonly API = `${environment.apiUrl}/users`
   private readonly TOKEN = `${environment.TOKEN}`
+  
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
@@ -19,6 +20,22 @@ export class UsersService {
 
   createUser(user: User): Observable<User> {
     return this.http.post<User>(this.API, user, {
+      headers: {
+        Authorization: `Bearer ${this.TOKEN}`
+      }
+    });
+  }
+
+  editUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.API}/${user.id}`, user, {
+      headers: {
+        Authorization: `Bearer ${this.TOKEN}`
+      }
+    });
+  }
+
+  deleteUser(id: number): Observable<User> {
+    return this.http.delete<User>(`${this.API}/${id}`, {
       headers: {
         Authorization: `Bearer ${this.TOKEN}`
       }
