f15dfe1fc2c83b8d63a1f4c3bc4847f99c0ace92
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-09-02 12:19:09 -0300

    feat: add users, posts, sidebar-menu modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: '', redirectTo: '/usuarios', pathMatch: 'full' }, // Rota padrão
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarMenuComponent,
+    UsersComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
