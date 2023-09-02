8932f5e4faa02e16dfbc582cac8392a1b96a4cef
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-09-02 15:26:28 -0300

    feat: create crud method postsService

--- src/app/components/services/posts.service.ts
+++ src/app/components/services/posts.service.ts
@@ -1,11 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../model/post.interface';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { 
-    
+  private readonly API = `${environment.apiUrl}/posts`
+  private readonly TOKEN = `${environment.TOKEN}`
+
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.API);
   }
+
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.API, post, {
+      headers: {
+        Authorization: `Bearer ${this.TOKEN}`
+      }
+    });
+  }
+
+  editPost(post: Post): Observable<Post> {
+    return this.http.put<Post>(`${this.API}/${post.id}`, post, {
+      headers: {
+        Authorization: `Bearer ${this.TOKEN}`
+      }
+    });
+  }
+
+  deletePost(id: number): Observable<Post> {
+    return this.http.delete<Post>(`${this.API}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${this.TOKEN}`
+      }
+    });
+  }
+
 }
