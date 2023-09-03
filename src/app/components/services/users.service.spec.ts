0c2a45b881de64a87b24c676047eb0920d1bf2d7
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-09-03 17:55:40 -0300

    feat: add tests to users service

--- src/app/components/services/users.service.spec.ts
+++ src/app/components/services/users.service.spec.ts
@@ -1,16 +1,100 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService],
+    });
     service = TestBed.inject(UsersService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  const TEST_API_URL = 'https://gorest.co.in/public/v2/users';
+
+  it('should search users', () => {
+    const searchTerm = 'John';
+    const searchBy = 'name';
+
+    service.searchUsers(searchTerm, searchBy).subscribe((response: any) => {
+      expect(response).toBeDefined();
+      expect(response.status).toBe(200);
+      expect(response.body).toBeDefined();
+    });
+
+    const req = httpTestingController.expectOne(
+      `${TEST_API_URL}?${searchBy}=${searchTerm}`
+    );
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should get user by ID', () => {
+    const userId = 1;
+
+    service.getUserById(userId).subscribe((user) => {
+      expect(user).toBeDefined();
+    });
+
+    const req = httpTestingController.expectOne(
+      `${TEST_API_URL}/${userId}`
+    );
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should create user', () => {
+    const newUser = { name: 'John', email: 'john@example.com', gender: 'male', status: 'active' };
+
+    service.createUser(newUser).subscribe((user) => {
+      expect(user).toBeDefined();
+    });
+
+    const req = httpTestingController.expectOne(
+      `${TEST_API_URL}`
+    );
+    expect(req.request.method).toBe('POST');
+  });
+
+  it('should edit user', () => {
+    const editedUser = {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      gender: 'male',
+      status: 'active',
+    };
+
+    service.editUser(editedUser).subscribe((user) => {
+      expect(user).toBeDefined();
+    });
+
+    const req = httpTestingController.expectOne(
+      `${TEST_API_URL}/${editedUser.id}`
+    );
+    expect(req.request.method).toBe('PUT');
+  });
+
+  it('should delete user', () => {
+    const userId = 1;
+
+    service.deleteUser(userId).subscribe((response) => {
+      expect(response).toBeDefined();
+    });
+
+    const req = httpTestingController.expectOne(
+      `${TEST_API_URL}/${userId}`
+    );
+    expect(req.request.method).toBe('DELETE');
   });
 });
