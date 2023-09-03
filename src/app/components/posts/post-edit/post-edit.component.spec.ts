3c1187d6074c505b28863cdfda3d6b590b26b4be
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-09-03 17:27:43 -0300

    feat: add tests to posts edit component

--- src/app/components/posts/post-edit/post-edit.component.spec.ts
+++ src/app/components/posts/post-edit/post-edit.component.spec.ts
@@ -1,21 +1,88 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PostEditComponent } from './post-edit.component';
+import { PostsService } from '../../services/posts.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { Post } from '../../model/post.interface';
 
 describe('PostEditComponent', () => {
   let component: PostEditComponent;
   let fixture: ComponentFixture<PostEditComponent>;
+  let postsService: PostsService;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PostEditComponent]
+      declarations: [PostEditComponent],
+      imports: [FormsModule],
+      providers: [
+        {
+          provide: PostsService,
+          useValue: {
+            getPostById: (id: number) =>
+              of({
+                id: 1,
+                user_id: 1,
+                title: 'Post 1',
+                body: 'Body 1',
+              } as Post),
+            editPost: (post: Post) => of(post),
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: () => {},
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '1',
+              },
+            },
+          },
+        },
+      ],
     });
+
     fixture = TestBed.createComponent(PostEditComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    postsService = TestBed.inject(PostsService);
+    router = TestBed.inject(Router);
+    activatedRoute = TestBed.inject(ActivatedRoute);
   });
 
-  it('should create', () => {
+  it('should create the PostEditComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch post on ngOnInit', () => {
+    spyOn(postsService, 'getPostById').and.callThrough();
+
+    component.ngOnInit();
+
+    expect(postsService.getPostById).toHaveBeenCalledWith(1);
+    expect(component.post).toEqual({
+      id: 1,
+      user_id: 1,
+      title: 'Post 1',
+      body: 'Body 1',
+    } as Post);
+  });
+
+  it('should submit form and navigate to posts', () => {
+    spyOn(router, 'navigate');
+    spyOn(postsService, 'editPost').and.callThrough();
+
+    component.onSubmit();
+
+    expect(postsService.editPost).toHaveBeenCalledWith(component.post);
+    expect(router.navigate).toHaveBeenCalledWith(['/posts']);
+  });
+
 });
