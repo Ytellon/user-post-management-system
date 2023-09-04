bf4c3f78e9822824480289fdc368c6187cc98c15
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-09-04 17:21:45 -0300

    fix: tests app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { SidebarMenuComponent } from './components/sidebar-menu/sidebar-menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        MatSidenavModule,
+        BrowserAnimationsModule,
+        MatListModule,
+        MatToolbarModule,
+        MatIconModule,
+      ],
       declarations: [AppComponent, SidebarMenuComponent],
     })
   );
--- src/app/components/sidebar-menu/sidebar-menu.component.spec.ts
+++ src/app/components/sidebar-menu/sidebar-menu.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { SidebarMenuComponent } from './sidebar-menu.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatSidenavModule } from '@angular/material/sidenav';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 describe('SidebarMenuComponent', () => {
@@ -14,7 +17,16 @@ describe('SidebarMenuComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [SidebarMenuComponent],
-      imports: [RouterModule.forRoot([]), MatFormFieldModule, MatAutocompleteModule, MatSidenavModule],
+      imports: [
+        RouterModule.forRoot([]),
+        MatFormFieldModule,
+        MatAutocompleteModule,
+        MatSidenavModule,
+        BrowserAnimationsModule,
+        MatListModule,
+        MatToolbarModule,
+        MatIconModule,
+      ],
     });
 
     fixture = TestBed.createComponent(SidebarMenuComponent);
@@ -25,18 +37,30 @@ describe('SidebarMenuComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render two navigation links', () => {
+  it('should render six navigation links', () => {
     fixture.detectChanges();
 
     const menuElement = fixture.nativeElement;
     const links = menuElement.querySelectorAll('a');
 
-    expect(links.length).toBe(2);
+    expect(links.length).toBe(6);
 
-    expect(links[0].textContent).toContain('Users');
-    expect(links[0].getAttribute('routerLink')).toBe('/users');
+    expect(links[0].textContent).toContain('Usuários');
+    expect(links[0].getAttribute('routerLink')).toBe(null);
 
-    expect(links[1].textContent).toContain('Posts');
-    expect(links[1].getAttribute('routerLink')).toBe('/posts');
+    expect(links[1].textContent).toContain('Criar Usuários');
+    expect(links[1].getAttribute('routerLink')).toBe('/create-user');
+
+    expect(links[2].textContent).toContain('Todos Usuários');
+    expect(links[2].getAttribute('routerLink')).toBe('/users');
+
+    expect(links[3].textContent).toContain('Postagens');
+    expect(links[3].getAttribute('routerLink')).toBe('/posts');
+
+    expect(links[4].textContent).toContain('Criar Postagem');
+    expect(links[4].getAttribute('routerLink')).toBe('/create-post');
+
+    expect(links[5].textContent).toContain('Todas Postagens');
+    expect(links[5].getAttribute('routerLink')).toBe('/posts');
   });
 });
